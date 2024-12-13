98e4176ea5b2bdee96919b4483e52fc239d51045
Hasibul294 <hasibulhasan294@gmail.com>
2024-12-14 00:58:46 +0600

    clean up folder structure and add custom alias for styling

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import "@styles/globals.css";
+// import localFont from "next/font/local";
+
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+export const metadata: Metadata = {
+  title: "Home",
+  description: "Wizard",
+};
+
+export default function PublicRootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MotherPage = () => {
+    return (
+        <div className='bg-slate-400 mt-9'>This is the mother page</div>
+    )
+}
+
+export default MotherPage
